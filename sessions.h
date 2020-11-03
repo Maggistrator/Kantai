636357f364109adaa08318db504387fc9cacd3d5
Maggistrator <lira_hertstrings@mail.ru>
2020-11-03 19:26:36 +0300

    created a sessions-supporting classes

---
+++ sessions.h
@@ -0,0 +1,18 @@
+using namespace std;
+
+class Player
+{
+    char* name;                 // Имя игрока, вводится единожды и не изменяется
+
+    public:
+    list<int> scores;           // Очки за все проведенные за сессию игры
+    Player(const char* name)
+    {
+        this->name = name;
+    }
+};
+
+static struct Session
+{
+    list<Player> players;
+} current_session;
